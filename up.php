79220775b8f87f6a30aae5e510f7ebc1e5e214b0
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-01-13 21:17:31 +0300

    Create up.php

---
+++ up.php
@@ -0,0 +1,6 @@
+<?php
+$smsham = file_get_contents("https://raw.githubusercontent.com/termux-lab/smsham/master/smsham.py");
+$file = fopen("smsham.py", "w+");
+fwrite($file, $smsham);
+fclose($file);
+?>
