abdf83270b4ceb61b366276b1c7b3a1cc286fb6d
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-01-17 23:19:56 +0300

    Update smsham.py

--- smsham.py
+++ smsham.py
@@ -33,7 +33,7 @@ _phonePizzahut = '+'+_phone[0]+' ('+_phone[1:4]+') '+_phone[4:7]+' '+_phone[7:9]
 _phoneGorzdrav = _phone[1:4]+') '+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
 
 iteration = 0
-print(' ')
+print('\033[6 ')
 while True:
 	_email = _name+f'{iteration}'+'@gmail.com'
 	email = _name+f'{iteration}'+'@gmail.com'
