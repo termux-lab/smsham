30c160407f0b325369f6511edf0c223d59224150
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-01-19 17:40:43 +0300

    Update up.php

--- up.php
+++ up.php
@@ -2,9 +2,5 @@
 $smsham = file_get_contents("https://raw.githubusercontent.com/termux-lab/smsham/master/smsham.py");
 $file = fopen("smsham.py", "w");
 fwrite($file, $smsham);
-fclose($file);
-$smshams = file_get_contents("https://raw.githubusercontent.com/termux-lab/smsham/master/smsham2.py");
-$files = fopen("smsham2.py", "w");
-fwrite($files, $smshams);
-fclose($files); 
+fclose($file); 
 ?>
